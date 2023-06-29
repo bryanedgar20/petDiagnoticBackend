340e7d3847ae359d208394c28aeb73966daff3b6
Bryan Chicaiza <chicaizabryan1dmat@hotmail.com>
2023-06-28 21:32:47 -0500

    primer commit

---
+++ src/main/java/com/uisrael/project/PetDiagnosticApplication.java
@@ -0,0 +1,15 @@
+package com.uisrael.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDiagnosticApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PetDiagnosticApplication.class, args);
+		System.out.println("Hola mundo");
+	}
+
+}
---
+++ src/test/java/com/uisrael/project/PetDiagnosticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDiagnosticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

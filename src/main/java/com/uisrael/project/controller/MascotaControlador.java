4a896990439dc219c225e7a1b058e8b315242975
[bryanedgar20] <[chicaizabryan1dmat@hotmail.com]>
2023-07-16 13:51:30 -0500

    nuevo metodo en mascotacontrolador

--- src/main/java/com/uisrael/project/controller/MascotaControlador.java
+++ src/main/java/com/uisrael/project/controller/MascotaControlador.java
@@ -1,11 +1,19 @@
 package com.uisrael.project.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.uisrael.project.modelo.entity.Usuario;
 
 @RestController
 @RequestMapping("/usuario")
 public class MascotaControlador {
 
-	
+	@GetMapping("obtenerMascota/{idMascota}")
+	public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable("idMascota") int idMascota) {
+		return ResponseEntity.ok(null);
+	}
 }
